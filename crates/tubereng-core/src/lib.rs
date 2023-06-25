91955193741b0928dca70e1cc81f79f8577c6f60
Clément Sibille <clements+git@lisible.xyz>
2023-06-25 18:24:38 +0200

    core: Fix typo in Transform struct name

--- crates/tubereng-core/src/lib.rs
+++ crates/tubereng-core/src/lib.rs
@@ -1,7 +1,7 @@
 use tubereng_math::{quaternion::Quaternion, vector::Vector3f};
 
 #[derive(Debug, Clone)]
-pub struct Tranform {
+pub struct Transform {
     pub translation: Vector3f,
     pub scale: Vector3f,
     pub rotation: Quaternion,
