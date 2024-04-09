48e4d16401f66d75bd3af0b534de46ef7c002c7e
Clement Sibille <clements+git@lisible.xyz>
2024-04-10 00:02:38 +0900

    Add tubereng_image crate

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub use tubereng_core as core;
 pub use tubereng_ecs as ecs;
 pub use tubereng_engine as engine;
 pub use tubereng_gui as gui;
+pub use tubereng_image as image;
 pub use tubereng_input as input;
 pub use tubereng_math as math;
 pub use tubereng_renderer as renderer;
