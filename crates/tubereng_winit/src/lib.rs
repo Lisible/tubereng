4f42a7aaa64c18ab5b3b9327d3ddbc948bb53b74
Clement Sibille <clements+git@lisible.xyz>
2024-04-14 02:06:22 +0900

    Use web_time instead of std::time for wasm build

--- crates/tubereng_winit/src/lib.rs
+++ crates/tubereng_winit/src/lib.rs
@@ -2,6 +2,11 @@
 
 use std::sync::Arc;
 
+#[cfg(not(target_arch = "wasm32"))]
+use std::time::Instant;
+#[cfg(target_arch = "wasm32")]
+use web_time::Instant;
+
 use tubereng_engine::Engine;
 use tubereng_input::{keyboard::Key, mouse::Button, Input};
 use winit::{
